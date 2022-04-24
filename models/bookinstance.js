5d2b3a341b1d5988f3a2f153602d4924ae0b9eac
voldev8 <voldev@hotmail.com>
2022-04-24 19:16:06 -0400

    fixed: grabbing the date for bookinstance update form

--- models/bookinstance.js
+++ models/bookinstance.js
@@ -24,5 +24,10 @@ BookInstanceSchema.virtual('due_back_formatted').get(function () {
   return DateTime.fromJSDate(this.due_back).toLocaleString(DateTime.DATE_MED);
 });
 
+BookInstanceSchema.virtual('due_back_form').get(function () {
+  //format for form
+  return DateTime.fromJSDate(this.due_back).toISODate();
+});
+
 //Export model
 module.exports = mongoose.model('BookInstance', BookInstanceSchema);
