7c12aaf58b6c044092b6f03c36bf7accaecdf0ed
voldev8 <voldev@hotmail.com>
2022-04-24 12:30:27 -0400

    displaying library data

--- models/author.js
+++ models/author.js
@@ -1,4 +1,5 @@
 var mongoose = require('mongoose');
+const { DateTime } = require('luxon');
 
 var Schema = mongoose.Schema;
 
@@ -34,6 +35,18 @@ AuthorSchema.virtual('lifespan').get(function () {
     lifetime_string += this.date_of_death.getYear();
   }
   return lifetime_string;
+});
+
+AuthorSchema.virtual('date_of_birth_formatted').get(function () {
+  return this.date_of_birth
+    ? DateTime.fromJSDate(this.date_of_birth).toLocaleString(DateTime.DATE_MED)
+    : '';
+});
+
+AuthorSchema.virtual('date_of_death_formatted').get(function () {
+  return this.date_of_death
+    ? DateTime.fromJSDate(this.date_of_death).toLocaleString(DateTime.DATE_MED)
+    : '';
 });
 
 // Virtual for author's URL
